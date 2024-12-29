44a9852c12e34d2b98df5400fea6ef68cef952d1
botiuk <svetabotiuk@gmail.com>
2024-12-29 09:59:35 +0200

    add content for main page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 class MainController < ApplicationController
-  def index; end
+  def index
+    @articles = Article.where.not(published_at: nil).where.not('published_at > ?',
+                                                               DateTime.now).articles_ordered.first(5)
+  end
 end
